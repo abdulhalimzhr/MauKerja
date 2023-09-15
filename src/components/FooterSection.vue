d1ecc53a5d1ca407dcaf7597ac69b868bd7e6e39
abdulhalimzhr <sideloadoel@icloud.com>
2023-09-16 01:42:59 +0700

    update footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-light bg-opacity-90 dark:!bg-gray-900 p-4">
     <div class="container mx-auto">
-      <p class="text-center text-white dark:text-gray-200 text-sm">
+      <p class="text-center text-white dark:text-gray-200 text-xs">
         2023 - Created by
         <a
           href="https://abdulhalimzhr.com"
